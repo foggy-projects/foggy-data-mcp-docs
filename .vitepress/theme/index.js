2dcb1cfb1c36ac6e4c507716aac7dbbc8cfc1fc5
root <oldseasoul@163.com>
2026-05-28 18:22:47 +0800

    docs: hide whitepaper translation links until english release

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -2,9 +2,38 @@ import DefaultTheme from 'vitepress/theme'
 import DownloadButton from './components/DownloadButton.vue'
 import './custom.css'
 
+function installWhitepaperTranslationGuard() {
+  if (typeof window === 'undefined' || typeof document === 'undefined') {
+    return
+  }
+
+  const update = () => {
+    document.documentElement.classList.toggle(
+      'foggy-hide-translations',
+      window.location.pathname.includes('/zh/whitepaper/')
+    )
+  }
+
+  const wrapHistoryMethod = (name) => {
+    const original = window.history[name]
+
+    window.history[name] = function (...args) {
+      const result = original.apply(this, args)
+      queueMicrotask(update)
+      return result
+    }
+  }
+
+  update()
+  window.addEventListener('popstate', update)
+  wrapHistoryMethod('pushState')
+  wrapHistoryMethod('replaceState')
+}
+
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
+    installWhitepaperTranslationGuard()
     app.component('DownloadButton', DownloadButton)
   }
 }
